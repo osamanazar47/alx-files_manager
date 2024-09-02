f9625bf1b1f80787b0e356301c547663e9090e77
osamanazar47 <osamanazar47@gmail.com>
2024-09-02 16:25:16 +0300

    created user controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,39 @@
-#!/usr/bin/node
+/* eslint-disable import/no-named-as-default */
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default class UsersCotroller {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      res.status(400).error('Missing email');
+    }
+    if (!password) {
+      res.status(400).error('Missing password');
+    }
+
+    try {
+      const existUser = dbClient.db.collection().findOne({ email });
+      if (existUser) {
+        res.status(400).json({ error: 'Already exists' });
+      }
+
+      const hashedPassword = sha1(password);
+
+      const result = await dbClient.db.collection('users').insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      const newUser = {
+        id: result.insertedId,
+        email,
+      };
+
+      return res.status(201).json(newUser);
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(400).json({ error: 'Internal Server Error' });
+    }
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersCotroller from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersCotroller.postNew);
 
 export default router;
