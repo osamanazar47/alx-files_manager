c12d0da3accc93be3bae16ad82a2c8a25ab6b4d5
osamanazar47 <osamanazar47@gmail.com>
2024-09-04 10:25:25 +0300

    create the files controller and init done with the upload files method

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,1 +1,85 @@
-#!/usr/bin/node
+/* eslint-disable import/no-named-as-default */
+import fs from 'fs';
+import path from 'path';
+import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+export default class FilesController {
+  static async postUpload(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await (await dbClient.usersCollection()).findOne({ _id: new ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const acceptedTypes = ['folder', 'file', 'image'];
+    // eslint-disable-next-line object-curly-newline
+    const { name, type, parentId = '0', isPublic = false, data } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Missing name' });
+    }
+    if (!type || !acceptedTypes.includes(type)) {
+      return res.status(400).json({ error: 'Missing type' });
+    }
+    if (type !== 'folder' && !data) {
+      return res.status(400).json({ error: 'Missing data' });
+    }
+
+    if (parentId !== '0') {
+      const parentFile = await (await dbClient.filesCollection())
+        .findOne({ _id: new ObjectId(parentId) });
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+      if (parentFile.type !== 'folder') {
+        return res.status(400).json({ error: 'Parent is not a folder' });
+      }
+    }
+
+    const newFile = {
+      userId: new ObjectId(userId),
+      name,
+      type,
+      isPublic,
+      parentId: parentId === '0' ? '0' : new ObjectId(parentId),
+    };
+
+    if (type === 'folder') {
+      const result = await (await dbClient.filesCollection()).insertOne(newFile);
+      return res.status(201).json({ id: result.insertedId, ...newFile });
+    }
+
+    const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+    const fileUUID = uuidv4();
+    const filePath = path.join(folderPath, fileUUID);
+    const fileData = Buffer.from(data, 'base64');
+
+    try {
+      fs.writeFileSync(filePath, fileData);
+    } catch (err) {
+      return res.status(500).json({ error: 'Could not save the file' });
+    }
+
+    newFile.localPath = filePath;
+
+    try {
+      const result = await (await dbClient.filesCollection()).insertOne(newFile);
+      return res.status(201).json({ id: result.insertedId, ...newFile });
+    } catch (err) {
+      return res.status(500).json({ error: 'Could not save file to the database' });
+    }
+  }
+}
